94a408ddff1cc1883b19b431b5b28ddf0dc13067
AxelSxidt <ax@gmail.com>
2024-01-16 21:01:57 -0600

    Se agregaron los forms y list  de pacientes

--- Coder/forms.py
+++ Coder/forms.py
@@ -6,3 +6,9 @@ class PodolodoForm(forms.ModelForm):
     class Meta:
         model = models.Podologo
         fields = "__all__"
+
+class PacienteForm(forms.ModelForm):
+    class Meta:
+        model = models.Paciente
+        fields = "__all__"
+
